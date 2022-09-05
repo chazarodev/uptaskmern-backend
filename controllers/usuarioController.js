ec1f0438294256e89a2fb2a9595318ee2a9bdcdd
Alberto Cházaro <shazarerom@gmail.com>
2022-09-04 21:34:53 -0500

    Creando endpoint para autenticación de usuario y validar datos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,8 @@
 import Usuario from "../models/Usuario.js";
+import generarID from "../helpers/generarID.js";
 
+
+//Función para registrar al usuario
 const registrar = async (req, res) => {
 
     //Evitar usuarios duplicados
@@ -13,13 +16,36 @@ const registrar = async (req, res) => {
 
     try {
         const usuario = new Usuario(req.body);
-        const usuarioAlmacenado = await usuario.save() 
+        usuario.token = generarID();
+        const usuarioAlmacenado = await usuario.save(); 
         res.json(usuarioAlmacenado);
     } catch (error) {
         console.error(error);
     }
 }
 
+//Función para autenticar al usuario
+const autenticar = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    //Comprobar si el usuario existe
+    const usuario = await Usuario.findOne({email});
+    if (!usuario) {
+        const error = new Error("El usuario no existe");
+        return res.status(404).json({msg: error.message});
+    }
+    
+    //Confirmar si está confirmado
+    if (!usuario.confirmado) {
+        const error = new Error("Tu cuenta no ha sido confirmada");
+        return res.status(403).json({msg: error.message});
+    }
+
+    //Comprobar su password
+}
+
 export {
-    registrar
+    registrar,
+    autenticar
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registrar } from "../controllers/usuarioController.js"
+import { registrar, autenticar } from "../controllers/usuarioController.js"
 
 const router = express.Router();
 
 //autenticación, registro y confirmación de usuarios
-router.post('/', registrar)//Crea un nuevo usuario
+router.post('/', registrar);//Crea un nuevo usuario
+router.post('/login', autenticar);
 
 
 export default router;
