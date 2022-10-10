24f137017971200692bf022af1441c7a5eed50ca
Alberto Cházaro <shazarerom@gmail.com>
2022-10-10 16:33:53 -0500

    Agregando socket.io para aplicación en tiempo real

--- index.js
+++ index.js
@@ -35,6 +35,50 @@ app.use("/api/tareas", tareaRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+const servidor = app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+//Socket.io
+import {Server} from 'socket.io'
+
+const io = new Server(servidor, {
+    pingTimeout: 6000,
+    cors: {
+        origin: process.env.FRONTEND_URL,
+    },
+});
+
+io.on('connection', (socket) => {
+    // console.log('Conectado a socket.io');
+
+    // Definir los eventos de socket.io
+    // socket.on('prueba', () => {
+    //     console.log('Putos');
+    //     socket.emit('respuesta');
+    // })
+
+    socket.on('abrir proyecto', (proyecto) => {
+        socket.join(proyecto);
+    })
+
+    socket.on('nueva tarea', tarea => {
+        const proyecto = tarea.proyecto;
+        socket.to(proyecto).emit('tarea agregada', tarea); //En lugar de .on utilizamos .to para evitar el error en el servidor
+    })
+
+    socket.on('eliminar tarea', tarea => {
+        const proyecto = tarea.proyecto;
+        socket.to(proyecto).emit('tarea eliminada', tarea);
+    })
+
+    socket.on('actualizar tarea', tarea => {
+        const proyecto = tarea.proyecto._id;
+        socket.to(proyecto).emit('tarea actualizada', tarea);
+    })
+    
+    socket.on('cambiar estado', tarea => {
+        const proyecto = tarea.proyecto._id;
+        socket.to(proyecto).emit('nuevo estado', tarea);
+    })
+})
